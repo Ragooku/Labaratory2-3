7905989c2c0439e449338b2e345ec81dca553b01
Vladislav <64515222+Ragooku@users.noreply.github.com>
2025-03-20 12:00:48 +0400

    upload gallery

--- scripts/sl.js
+++ scripts/sl.js
@@ -3,20 +3,20 @@ let imageIndex = 0;
 function show(index) {    images[imageIndex].classList.remove('active');
     images[index].classList.add('active');    imageIndex = index;
 }
-controlls.forEach((e) => {    
+controlls.forEach((e) => {
     e.addEventListener('click', () => {
-        if (event.target.classList.contains('prev')) {     
-                   let index = imageIndex - 1;
+        if (event.target.classList.contains('prev')) {
+            let index = imageIndex - 1;
             if (index < 0) {
                 index = images.length - 1;            }
             show(index);
-        } else if (event.target.classList.contains('next')) {         
-               let index = imageIndex + 1;
-            if (index >= images.length) {            
-                    index = 0;
-            }            
+        } else if (event.target.classList.contains('next')) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
             show(index);
-        }   
-     })
+        }
+    })
 })
 show(imageIndex);
